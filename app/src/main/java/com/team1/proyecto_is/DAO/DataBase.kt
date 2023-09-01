4a0809b150c70b56b35863808fa08f5b9d00d8a5
JuanMBV <juanmbv.31@gmail.com>
2023-09-01 17:37:36 -0600

    Data Base implemented

---
+++ app/src/main/java/com/team1/proyecto_is/DAO/DataBase.kt
@@ -0,0 +1,52 @@
+package com.team1.proyecto_is.DAO
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+
+class DataBase(context: Context?): SQLiteOpenHelper(context, "Ahora_Si.db", null, 1) {
+    override fun onCreate(db: SQLiteDatabase) {
+        // Crear la tabla Plantillas
+        db.execSQL(
+            "CREATE TABLE plantillas ("
+                    + "id_plantilla INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "nombre TEXT NOT NULL"
+                    + ")"
+        )
+
+        // Crear la tabla Eventos
+        db.execSQL(
+            "CREATE TABLE eventos ("
+                    + "id_eventos INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "id_plantilla INTEGER,"
+                    + "id_status INTEGER,"
+                    + "materia TEXT,"
+                    + "parte_cuerpo TEXT,"
+                    + "descripcion TEXT,"
+                    + "comida TEXT,"
+                    + "lugar TEXT,"
+                    + "fecha_inicial TEXT,"
+                    + "fecha_recordatorio TEXT,"
+                    + "timer TEXT,"
+                    + "FOREIGN KEY(id_plantilla) REFERENCES plantillas(id_plantilla)"
+                    + ")"
+        )
+
+        // Crear la tabla Estatus
+        db.execSQL(
+            "CREATE TABLE status ("
+                    + "id_status INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "fecha_registro TEXT NOT NULL"
+                    + ")"
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // Manejar actualizaciones de la base de datos si es necesario
+        db.execSQL("DROP TABLE IF EXISTS plantillas")
+        db.execSQL("DROP TABLE IF EXISTS eventos")
+        db.execSQL("DROP TABLE IF EXISTS status")
+        onCreate(db)
+    }
+}
--- app/src/main/java/com/team1/proyecto_is/MainActivity.kt
+++ app/src/main/java/com/team1/proyecto_is/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.team1.proyecto_is
 
+import android.content.Context
 import android.os.Bundle
+import android.provider.ContactsContract.Data
+import android.telephony.mbms.MbmsErrors.InitializationErrors
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,8 +13,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.team1.proyecto_is.DAO.DataBase
 import com.team1.proyecto_is.navigation.AppNavigation
-import com.team1.proyecto_is.screen.SelectTemplate
 import com.team1.proyecto_is.ui.theme.ProyectoISTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,11 +27,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    // Initialize Data Base
+                    val db = InitializeDatabaseConnection(this)
+
                     AppNavigation()
                 }
             }
         }
     }
+}
+
+fun InitializeDatabaseConnection(context: Context): DataBase{
+    val db = DataBase(context)
+    return db
 }
 
 @Preview(showSystemUi = true)
