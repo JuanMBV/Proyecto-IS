327b784ae2abe4f20ef6d1c6a64800b4be5185f6
Diana Zapata <dianarocio.czapata@gmail.com>
2023-09-12 12:28:18 -0600

    implementado de splash screen :3c

--- app/src/main/java/com/team1/proyecto_is/navigation/AppNavigation.kt
+++ app/src/main/java/com/team1/proyecto_is/navigation/AppNavigation.kt
@@ -6,12 +6,18 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.team1.proyecto_is.DAO.DataBase
 import com.team1.proyecto_is.screen.SelectTemplate
+import com.team1.proyecto_is.screen.SplashScreen
 import com.team1.proyecto_is.screen.ViewEvents
 
 @Composable
 fun AppNavigation(dataBase: DataBase){
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = AppScreens.SelectTemplate.route){
+    NavHost(
+        navController = navController,
+        startDestination = AppScreens.SplashScreen.route){
+        composable(route = AppScreens.SplashScreen.route){
+            SplashScreen(navController = navController)
+        }
         composable(route = AppScreens.SelectTemplate.route){
             SelectTemplate(navController)
         }
--- app/src/main/java/com/team1/proyecto_is/navigation/AppScreens.kt
+++ app/src/main/java/com/team1/proyecto_is/navigation/AppScreens.kt
@@ -3,4 +3,5 @@ package com.team1.proyecto_is.navigation
 sealed class AppScreens (val route: String){
     object SelectTemplate: AppScreens("select_template")
     object ViewEvents: AppScreens("view_events")
+    object SplashScreen: AppScreens("splash_screen")
 }
